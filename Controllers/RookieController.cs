3c610bd271ae24969eedd0e721c9e7b13a473c08
Quan Do <minhquand3@gmail.com>
2023-06-23 12:17:15 +0700

    Added A1E4

--- Controllers/RookieController.cs
+++ Controllers/RookieController.cs
@@ -26,4 +26,42 @@ public class RookieController : Controller
 
     return View(males);
   }
+
+  public IActionResult A1E2()
+  {
+    var oldest = (from p in _context.Person orderby p.DateOfBirth select p).FirstOrDefault();
+
+    if (oldest == null)
+    {
+      return NotFound();
+    }
+
+    return View(oldest);
+  }
+
+  public IActionResult A1E3()
+  {
+    var fullname = from p in _context.Person select p.FullName;
+
+    return View(fullname);
+  }
+
+  public IActionResult A1E4(int query = 0)
+  {
+    var year2000 = from p in _context.Person where p.DateOfBirth.Year == 2000 select p;
+    ViewData["query"] = query;
+    switch (query)
+    {
+      case 0:
+        return View(year2000);
+      case 1:
+        var more2000 = from p in _context.Person where p.DateOfBirth.Year > 2000 select p;
+        return View(more2000);
+      case 2:
+        var less2000 = from p in _context.Person where p.DateOfBirth.Year < 2000 select p;
+        return View(less2000);
+      default:
+        return View(year2000);
+    }
+  }
 }
