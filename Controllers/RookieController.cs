19081c026a5be88a33a2856e7d534e0978499b73
Quan Do <minhquand3@gmail.com>
2023-06-23 11:23:19 +0700

    Added A1E1

--- Controllers/RookieController.cs
+++ Controllers/RookieController.cs
@@ -1,20 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
-
 using MyWebApp.Models;
+using MyWebApp.Data;
 
 namespace MyWebApp.Controllers;
 
 public class RookieController : Controller
 {
   private readonly ILogger<RookieController> _logger;
+  private readonly PersonContext _context;
 
-  public RookieController(ILogger<RookieController> logger)
+  public RookieController(ILogger<RookieController> logger, PersonContext context)
   {
     _logger = logger;
+    _context = context;
   }
 
   public IActionResult Index()
   {
     return View();
+  }
+
+  public IActionResult A1E1()
+  {
+    var males = from p in _context.Person where p.Gender == "Male" select p;
+
+    return View(males);
   }
 }
